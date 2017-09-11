05412824b8f2137f5333919725693e48f89a04a1
C4TFLY <linus.holmstrom@hotmail.com>
2017-09-11 16:12:53 +0200

    Started work

---
+++ Assets/Code/AtomManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtomManager : MonoBehaviour {
+    
+    public static void Merge(GameObject[] atoms)
+    {
+        GameObject newParent = new GameObject("Deuterium");
+        for (int i = 0; i < atoms.Length; i++)
+        {
+            atoms[i].transform.SetParent(newParent.transform);
+        }
+    }
+
+}
---
+++ Assets/Code/Temp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour {
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        List<GameObject> go = new List<GameObject>
+        {
+            collision.gameObject,
+            gameObject
+        };
+
+        AtomManager.Merge(go.ToArray());
+    }
+
+}
