eea18f689b48e9ea188218a0906d16f45d41be6e
C4TFLY <linus.holmstrom@hotmail.com>
2017-09-11 15:01:54 +0200

    Initial commit

---
+++ Assets/Code/Editor/AutoSave.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutoSave {
+
+    static AutoSave()
+    {
+        EditorApplication.playmodeStateChanged += OnPlayModeStateChanged;
+    }
+
+    static void OnPlayModeStateChanged()
+    {
+        if (EditorApplication.isPlaying == false)
+        {
+            EditorSceneManager.SaveOpenScenes();
+        }
+    }
+
+}
