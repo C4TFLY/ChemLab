149b7a065982ab5a200db76123135140ca2f2f88
C4TFLY <linus.holmstrom@hotmail.com>
2017-09-18 16:20:50 +0200

    Floating added
    Created script for atoms to float around within the secene.
    Bouncing has been started, but needs some tweaks to the direction they bounce.

---
+++ Assets/Code/Controllers/FloatingController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FloatingController : MonoBehaviour {
+
+    private float moveSpeed;
+    private Vector3 initialDir;
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+    private float deltaTime;
+
+    private void Start()
+    {
+
+#region Vectors
+
+        initialDir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+
+        #endregion
+
+#region Components
+
+        rb = GetComponent<Rigidbody>();
+
+        #endregion
+
+#region Variable assignment
+
+        moveDirection = initialDir;
+        deltaTime = GameManager.deltaTime;
+        moveSpeed = Random.Range(0.3f, 0.7f);
+
+#endregion
+
+    }
+
+    private void FixedUpdate()
+    {
+        deltaTime = GameManager.deltaTime;
+        rb.MovePosition(transform.position + moveDirection * deltaTime * moveSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        moveDirection = collision.contacts[0].normal;
+        moveDirection = Quaternion.AngleAxis(Random.Range(-70f, 70f), Vector3.forward) * moveDirection;
+    }
+
+}
---
+++ Assets/Code/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static float deltaTime
+    {
+        get;
+        private set;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        deltaTime = Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
