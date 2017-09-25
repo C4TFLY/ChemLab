c3896913f98f5703084ae91cefd051aabc5d92a3
C4TFLY <linus.holmstrom@hotmail.com>
2017-09-25 16:17:46 +0200

    Custom min max slider
    Bouncing fixed, and custom slider for min/max move speed added

--- Assets/Code/Controllers/FloatingController.cs
+++ Assets/Code/Controllers/FloatingController.cs
@@ -5,47 +5,34 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class FloatingController : MonoBehaviour {
 
+    public float minMoveSpeed = 0.3f;
+    public float maxMoveSpeed = 0.7f;
+
     private float moveSpeed;
     private Vector3 initialDir;
     private Rigidbody rb;
     private Vector3 moveDirection;
-    private float deltaTime;
 
     private void Start()
     {
-
-#region Vectors
-
         initialDir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
-
-        #endregion
-
-#region Components
+        
 
         rb = GetComponent<Rigidbody>();
-
-        #endregion
-
-#region Variable assignment
+        
 
         moveDirection = initialDir;
-        deltaTime = GameManager.deltaTime;
-        moveSpeed = Random.Range(0.3f, 0.7f);
-
-#endregion
-
+        moveSpeed = Random.Range(minMoveSpeed, maxMoveSpeed);
     }
 
     private void FixedUpdate()
     {
-        deltaTime = GameManager.deltaTime;
-        rb.MovePosition(transform.position + moveDirection * deltaTime * moveSpeed);
+        rb.MovePosition(transform.position + moveDirection * Time.deltaTime * moveSpeed);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        moveDirection = collision.contacts[0].normal;
-        moveDirection = Quaternion.AngleAxis(Random.Range(-70f, 70f), Vector3.forward) * moveDirection;
+        moveDirection = Vector3.Reflect(moveDirection, collision.contacts[0].normal);
     }
 
 }
---
+++ Assets/Code/Editor/FloatingControllerEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FloatingController))]
+[CanEditMultipleObjects]
+public class FloatingControllerEditor : Editor {
+
+    float minMoveSpeed;
+    float maxMoveSpeed;
+    FloatingController floatingController;
+
+    private void OnEnable()
+    {
+        minMoveSpeed = serializedObject.FindProperty("minMoveSpeed").floatValue;
+        maxMoveSpeed = serializedObject.FindProperty("maxMoveSpeed").floatValue;
+        floatingController = (FloatingController)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+
+        EditorGUILayout.MinMaxSlider(ref minMoveSpeed, ref maxMoveSpeed, 0.1f, 1f);
+
+        floatingController.minMoveSpeed = minMoveSpeed;
+        floatingController.maxMoveSpeed = maxMoveSpeed;
+    }
+
+}
