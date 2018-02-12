5d6761740a9b9cd9592997dcfb3a491353478476
Linus Holmström <linus.holmstrom@hotmail.com>
2018-02-12 15:48:07 +0100

    Database modifications and upgrades
    Updated to unity 2017.3.1f.
    Added database complete with isotopes, protons and so on...
    Started simple script to add the amount of neutrons to the DB as well.

--- Assets/Code/DB/Temp.cs
+++ Assets/Code/DB/Temp.cs
@@ -1,36 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mono.Data.Sqlite;
 using System;
+using Mono.Data.Sqlite;
 using System.Data;
+using System.Linq;
 
-public class DBTest : MonoBehaviour {
+public class Temp : MonoBehaviour {
 
+    Dictionary<string, int> neutrons;
+
+	// Use this for initialization
 	void Start () {
+        neutrons = new Dictionary<string, int>();
         string conn = "URI=file:" + Application.dataPath + "/Other/scraped.db";
-        print(conn);
         IDbConnection dbconn;
         dbconn = new SqliteConnection(conn);
         dbconn.Open();
         IDbCommand dbcmd = dbconn.CreateCommand();
 
-        string sqlQuery = "SELECT name, isotope, chemicalName, protons FROM Scraped LIMIT 100";
+        string sqlQuery = "SELECT name, isotope, protons FROM Scraped";
         dbcmd.CommandText = sqlQuery;
         IDataReader reader = dbcmd.ExecuteReader();
 
         while (reader.Read())
         {
+            int protons = reader.GetInt32(2);
             string name = reader.GetString(0);
             string isotope = reader.GetString(1);
-            string chemicalName = reader.GetString(2);
-            int protons = reader.GetInt32(3);
 
-            Debug.Log("<b>Name:</b> " + name);
-            Debug.Log("<b>Isotope:</b> " + isotope);
-            Debug.Log("<b>Chemical Name:</b> " + chemicalName);
-            Debug.Log("<b>Protons:</b> " + protons);
-            Debug.Log("--------------------------------------------------");
+            string massNumberString = new String(isotope.Where(c => Char.IsDigit(c)).ToArray());
+            int massNumber = Int32.Parse(massNumberString);
+            int neutrons = massNumber - protons;
+
+
+            asdasd
+
+            //TODO: Add mass number with isotope to dictionary, then make a new query outside of reader.Read()
         }
 
         reader.Close();
@@ -39,5 +45,10 @@ public class DBTest : MonoBehaviour {
         dbcmd = null;
         dbconn.Close();
         dbconn = null;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
 }
