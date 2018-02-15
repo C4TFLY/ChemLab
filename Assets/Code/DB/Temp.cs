fd96db8e9822cd7ec70ca57d859a800f4d2ae3f9
Linus Holmström <linus.holmstrom@hotmail.com>
2018-02-15 08:09:47 +0100

    stuff

--- Assets/Code/DB/Temp.cs
+++ Assets/Code/DB/Temp.cs
@@ -7,40 +7,43 @@ using System.Data;
 using System.Linq;
 
 public class Temp : MonoBehaviour {
-
-    Dictionary<string, int> neutrons;
-
+    
 	// Use this for initialization
 	void Start () {
-        neutrons = new Dictionary<string, int>();
+
+        Dictionary<string, int> neutronDict = new Dictionary<string, int>();
         string conn = "URI=file:" + Application.dataPath + "/Other/scraped.db";
         IDbConnection dbconn;
         dbconn = new SqliteConnection(conn);
         dbconn.Open();
         IDbCommand dbcmd = dbconn.CreateCommand();
 
-        string sqlQuery = "SELECT name, isotope, protons FROM Scraped";
+        string sqlQuery = "SELECT isotope, protons FROM Scraped";
         dbcmd.CommandText = sqlQuery;
         IDataReader reader = dbcmd.ExecuteReader();
-
         while (reader.Read())
         {
-            int protons = reader.GetInt32(2);
-            string name = reader.GetString(0);
-            string isotope = reader.GetString(1);
+            int protons = reader.GetInt32(1);
+            string isotope = reader.GetString(0);
 
             string massNumberString = new String(isotope.Where(c => Char.IsDigit(c)).ToArray());
             int massNumber = Int32.Parse(massNumberString);
             int neutrons = massNumber - protons;
 
-
-            asdasd
-
+            neutronDict.Add(isotope, neutrons);
             //TODO: Add mass number with isotope to dictionary, then make a new query outside of reader.Read()
         }
-
         reader.Close();
         reader = null;
+
+        foreach(KeyValuePair<string, int> entry in neutronDict)
+        {
+            sqlQuery = "UPDATE Scraped SET neutrons = " + entry.Value + " WHERE isotope = '" + entry.Key + "'";
+            dbcmd.CommandText = sqlQuery;
+            dbcmd.ExecuteNonQuery();
+        }
+
+
         dbcmd.Dispose();
         dbcmd = null;
         dbconn.Close();
